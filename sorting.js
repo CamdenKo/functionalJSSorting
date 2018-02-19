e17591442098d4b798b0eca957b274b4e4ca1634
Camden Ko <camdenko@gmail.com>
2018-02-19 01:10:09 -0600

    mergesort

--- sorting.js
+++ sorting.js
@@ -15,6 +15,43 @@ const bubbleSort = (arr) => {
   return arr
 }
 
+const merge = (arr1, arr2) => {
+  const result = []
+  let leftIndex = 0
+  let rightIndex = 0
+
+  while (leftIndex < arr1.length && rightIndex < arr2.length) {
+    if (arr1[leftIndex] < arr2[rightIndex]) {
+      result.push(arr1[leftIndex])
+      leftIndex++
+    } else {
+      result.push(arr2[rightIndex])
+      rightIndex++
+    }
+  }
+
+  return [
+    ...result,
+    ...arr1.slice(leftIndex),
+    ...arr2.slice(rightIndex),
+  ]
+}
+
+const mergeSort = (arr) => {
+  if (!arr.length || arr.length === 1) {
+    return arr
+  }
+
+  const middle = Math.floor(arr.length / 2)
+  const left = arr.slice(0, middle)
+  const right = arr.slice(middle)
+  return merge(
+    mergeSort(left),
+    mergeSort(right),
+  )
+}
+
 module.exports = {
   bubbleSort,
+  mergeSort,
 }
