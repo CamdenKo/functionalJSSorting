8eb14a852bc8a28eec3f0aefebeee23645a04536
Camden Ko <camdenko@gmail.com>
2018-02-19 00:58:10 -0600

    bubbleswap completed

--- sorting.js
+++ sorting.js
@@ -1,7 +1,20 @@
-const test = a => {
-  console.log(a)
-  return []
+const swap = (arr, index1, index2) => {
+  const temp = arr[index1]
+  arr[index1] = arr[index2]
+  arr[index2] = temp
 }
+
+const bubbleSort = (arr) => {
+  for (let first = 0; first < arr.length - 1; first++) {
+    for (let second = first + 1; second < arr.length; second++) {
+      if (arr[first] > arr[second]) {
+        swap(arr, first, second)
+      }
+    }
+  }
+  return arr
+}
+
 module.exports = {
-  test,
+  bubbleSort,
 }
